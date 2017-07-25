6df232089cb6aca9628f3ccf15a621639f617ae8
saikishan <saikishan2008@gmail.com>
2017-07-25 23:51:02 +0530

    now at page no 89 have to do athinticate social posts

--- controllers/static.js
+++ controllers/static.js
@@ -3,10 +3,11 @@ var path = require('path');
 var router = express.Router();
 router.get('/',function(req,res){
     console.log("serving web page");
-    res.sendFile(path.join(__dirname,'../Layouts','index.html'));
+    res.sendFile(path.join(__dirname,'../Layouts','app.html'));
 });
 router.get('/favicon.ico',function(req,res){
     res.sendfile(path.join(__dirname,'../Layouts','favicon.ico'));
 })
+router.use(express.static(__dirname+'/../templates'));
 router.use(express.static(__dirname+'/../assets'));
 module.exports=router;
